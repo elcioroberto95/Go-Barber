ccb983f8648e69644378bcaf12b0c2be82544264
elcioroberto95 <elcioroberto1995@gmail.com>
2021-02-23 23:39:26 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "@jungsoft/eslint-config/react",
+    "@jungsoft/eslint-config/typescript",
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "no-underscore-dangle": "off",
+    camelcase: "off",
+    "react/jsx-filename-extension": "off",
+    "import/order": "off",
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {},
+      node: {
+        extensions: [
+          ".ts",
+          ".tsx",
+        ],
+      },
+    },
+  },
+};
